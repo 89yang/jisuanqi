c770feb6ba9484d192110bbcf14920b6c21ce3a8
89yang <56387769+89yang@users.noreply.github.com>
2019-10-16 16:51:14 +0800

    测试

---
+++ text/src/text/qwe.java
@@ -0,0 +1,13 @@
+package text;
+
+import java.util.Scanner;
+
+public class qwe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+	}
+
+}
